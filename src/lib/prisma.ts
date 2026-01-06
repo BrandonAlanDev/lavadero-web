a83ee6ec1475485cec065c85a86e44ded7c321ec
valentin <valentinmendez0042@gmail.com>
2026-01-06 17:35:31 -0300

    cambios para que prisma genere los schema

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,7 +7,7 @@ const adapter = new PrismaMariaDb({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  ssl: {
+ ssl: {
     rejectUnauthorized: true
   },
   connectionLimit: 5,
